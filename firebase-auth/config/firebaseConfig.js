3b977f9e2358e61a6c9cf8168dffd054252a774f
Parfait <jenny.filcheck@gmail.com>
2017-06-23 21:23:37 -0500

    basic react-native firebase auth workflow

---
+++ firebase-auth/config/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyADxW9nUn5LlQPZpRAdNc4vREWGQotSmjA",
+  authDomain: "react-native-auth-74b98.firebaseapp.com",
+  databaseURL: "https://react-native-auth-74b98.firebaseio.com",
+  projectId: "react-native-auth-74b98",
+  storageBucket: "react-native-auth-74b98.appspot.com",
+  messagingSenderId: "829006210783"
+};
---
+++ firebase-auth/src/components/common/index.js
@@ -0,0 +1,8 @@
+import Button from './button';
+import Card from './card';
+import CardSection from './cardSection';
+import Header from './header';
+import FormField from './formField';
+import Spinner from './spinner';
+
+export { Button, Card, CardSection, Header, FormField, Spinner };
